7440db57db6b700f402956fa24c8b1ba7621fa14
Bingnan Li, Nathan <leebingnam@hotmail.com>
2018-05-22 00:41:15 -0700

    RTS Camera, Click mouse to move

---
+++ Unity_CaveAdventure/Assets/02_Scripts/SetNewLocation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class SetNewLocation : MonoBehaviour {
+
+    public Transform posIndicator;
+    public Transform crowd;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SetTargetLocation();
+        }
+
+
+
+
+    }
+
+    private void SetTargetLocation()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            posIndicator.position = hit.point;
+
+        foreach (Transform child in crowd)
+            child.GetComponent<AICharacterControl>().SetTarget(posIndicator);
+    }
+}
